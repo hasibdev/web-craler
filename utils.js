06813e81647bb832ac0eff04afc64bdbb777b5ad
hasibdev <hasib.active@gmail.com>
2023-05-31 23:45:46 +0600

    2 files modified

--- utils.js
+++ utils.js
@@ -4,7 +4,11 @@ function sum(a, b) {
 
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
-  return `${urlObj.hostname}${urlObj.pathname}`;
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length && hostPath.slice(-1) === '/')
+    return hostPath.slice(0, -1);
+
+  return hostPath;
 }
 
 module.exports = {
--- utils.test.js
+++ utils.test.js
@@ -2,7 +2,7 @@ const { sum, normalizeURL } = require('./utils.js');
 const { test, expect, describe } = require('@jest/globals');
 
 describe('sum module', () => {
-  test('1+3 to equal 4', () => {
+  test('2+2 to equal 4', () => {
     const inputA = 2;
     const inputB = 2;
     const result = sum(inputA, inputB);
@@ -10,11 +10,33 @@ describe('sum module', () => {
 
     expect(result).toBe(expected);
   });
+  test('5+5 to equal 10', () => {
+    const inputA = 5;
+    const inputB = 5;
+    const result = sum(inputA, inputB);
+    const expected = 10;
+
+    expect(result).toBe(expected);
+  });
 });
 
 describe('normalize URL', () => {
-  test('normalize url', () => {
-    const input = 'https://hasib.dev/about';
+  test('check protocol', () => {
+    const input = 'https://hasib.dev/about/';
+    const result = normalizeURL(input);
+    const expected = 'hasib.dev/about';
+
+    expect(result).toEqual(expected);
+  });
+  test('check query', () => {
+    const input = 'https://hasib.dev/about?id=2';
+    const result = normalizeURL(input);
+    const expected = 'hasib.dev/about';
+
+    expect(result).toEqual(expected);
+  });
+  test('check www', () => {
+    const input = 'https://www.hasib.dev/about?id=2';
     const result = normalizeURL(input);
     const expected = 'hasib.dev/about';
 
